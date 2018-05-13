048946aa058b040052c6ecde70b05738049c2fc5
julien <julinksnow@gmail.com>
2018-05-13 02:35:17 +0200

    Creating projects with Archetypes

---
+++ Archetype/noninteractively/jumpstart-example/src/main/java/Noninteractively/example/App.java
@@ -0,0 +1,13 @@
+package Noninteractively.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
